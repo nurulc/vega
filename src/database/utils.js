bbf52fc817d3bb8f6652d8d632808e3ab81480d6
vbojilova <viki.bojilova@gmail.com>
2019-02-19 11:58:16 -0500

    Added yaml/docker support

--- src/database/utils.js
+++ src/database/utils.js
@@ -4,32 +4,146 @@ import {
 } from "../resources/utils.js";
 const fs = require("fs");
 const appRoot = require("app-root-path");
-const rootPath = appRoot + "/src/database/temp/";
+var writeYaml = require("write-yaml");
+const exec = require("child_process").exec;
+const tempPath = appRoot + "/src/database/temp/";
 import {
   getFileTypeByFileName,
   getExpectedFileTargetByType
 } from "../js/CreateAnalysis/utils/utils";
-import {inputConfig} from "../resources/config";
+import {
+  dashboardConfig,
+  inputConfig,
+  dockerCommands
+} from "../resources/config";
 
 //Returns [analysisID,[fileIDs]] of a created analysis
-export const createAnalysis = async (database, params) => {
+export const createAnalysis = async (database, params, event) => {
   //Query the database for missing parsed files
-  var allFileIDs = await getAllFileIds(database, params);
-
+  var allFileIDs = createAllNewDBFiles(database, params, event);
   var analysisID = await getAnalysisID(
     database,
     params.name,
-    params.description
+    params.description,
+    params.jiraId
   );
-
   var finalAnalysis = await createAnalysisFileRelations(
     analysisID,
     allFileIDs,
     database
   );
 
-  return finalAnalysis;
+  var finalExecute = await attachDockerComponenets(
+    database,
+    analysisID,
+    params,
+    event
+  );
+  return finalExecute;
 };
+
+async function attachDockerComponenets(database, analysisID, params, event) {
+  var yamlFilePath = await createNewYamlVersion(
+    database,
+    analysisID,
+    params,
+    event
+  );
+
+  event.sender.send("test", yamlFilePath);
+  var finalExecute = await executeYamlLoad(yamlFilePath, event);
+  return finalExecute;
+}
+
+var lyraYamlFile = function(params) {
+  this.project = dashboardConfig.project;
+  this.title = params.name;
+  this.sample_ids = [params.name];
+  this.library_ids = [params.name];
+  this.analysis_id = params.name;
+  this.jira_id = params.jiraId;
+  this.description = params.description ? params.description : "";
+  this.files = {
+    segs: params.filePaths.segs,
+    tree: params.filePaths.tree[0]
+  };
+};
+const executeYamlLoad = async (yamlFilePath, event) => {
+  var loadYamlCommand = dockerCommands.pythonParseCommand.replace(
+    "{yaml}",
+    yamlFilePath
+  );
+
+  return new Promise((resolve, reject) => {
+    var load = exec(loadYamlCommand);
+    load.stderr.on("data", data => {
+      event.sender.send("error-WithMsg", data, 30000);
+    });
+
+    load.stdout.on("data", data => {
+      var liveOutput = data.toString();
+      if (liveOutput.indexOf("-TreeDone") !== -1) {
+        event.sender.send("analysisLoadingStep", liveOutput, "Tree");
+      } else if (liveOutput.indexOf("-SegDone") !== -1) {
+        event.sender.send("analysisLoadingStep", liveOutput, "Segment");
+      } else if (liveOutput.indexOf("-AnalysisDone") !== -1) {
+        event.sender.send("analysisLoadingStep", liveOutput, "Analysis");
+        resolve();
+      }
+    });
+  });
+};
+const createYamlMetaObject = (db, analysisID) => {
+  var hit = db.versions.find({analysisID: analysisID});
+  var version =
+    hit.length > 0
+      ? Math.max.apply(
+          Math,
+          hit.map(function(o) {
+            return o.version;
+          })
+        ) + 1
+      : 1;
+  var fileName = "id_" + analysisID + "_v_" + version + ".yml";
+  var filePath = tempPath + fileName;
+  return {
+    analysisID: analysisID,
+    version: version,
+    fileName: fileName,
+    filePath: filePath
+  };
+};
+const createYamlFile = async (yamlMetaObject, yamlObj, event) => {
+  return new Promise((resolve, reject) => {
+    writeYaml(yamlMetaObject.filePath, yamlObj, function(err) {
+      if (err) {
+        event.sender.send("error-WithMsg", err, 30000);
+        reject(err);
+      } else {
+        resolve(yamlMetaObject);
+      }
+    });
+  });
+};
+
+async function createNewYamlVersion(db, analysisID, params, event) {
+  var yamlObj = new lyraYamlFile(params);
+  var yamlMetaObject = createYamlMetaObject(db, analysisID);
+  event.sender.send("test", yamlMetaObject);
+  return new Promise(async (resolve, reject) => {
+    //create yamlfile
+    return await createYamlFile(yamlMetaObject, yamlObj, event).then(
+      //store analysis ID and yaml name
+      yamlMeta => {
+        event.sender.send("test", yamlMeta);
+        event.sender.send("test", "yamlMetaObject");
+        createDbYamlVersion(db, yamlMeta);
+        resolve(yamlMeta.filePath);
+      }
+    );
+  });
+}
+
 async function createAnalysisFileRelations(analysisID, allFileIDs, database) {
   //Once all files and analysis entries are created, make the relation entries
   return Promise.all([analysisID, allFileIDs]).then(function(databaseResults) {
@@ -42,12 +156,16 @@ async function createAnalysisFileRelations(analysisID, allFileIDs, database) {
     return results;
   });
 }
-async function getAllFileIds(database, params) {
+async function createAllNewDBFiles(database, params, event) {
+  var sortedFileIDList = getParsedFilePathObj(params.filePaths, event);
+  return createFileDBEntry(database, sortedFileIDList);
+}
+async function getAllFileIds(database, params, event) {
   var sortedFileIDList = sortFileEntries(database, params.filePaths);
   //Return the id's from the files table for each file
   return new Promise(async (resolve, reject) => {
     if (sortedFileIDList.missingDbEntries.length !== 0) {
-      await getMissingFileIds(sortedFileIDList, database).then(
+      await getMissingFileIds(sortedFileIDList, database, event).then(
         missingFileIds => {
           var allFileIds = missingFileIds.concat(
             sortedFileIDList.existingDbEntries
@@ -61,7 +179,7 @@ async function getAllFileIds(database, params) {
     }
   });
 }
-const getMissingFileIds = (fileEntries, database) => {
+const getMissingFileIds = (fileEntries, database, event) => {
   return Promise.all(
     //For every missing json file, parse and save
     fileEntries["missingDbEntries"].map(async filePathObj => {
@@ -71,16 +189,17 @@ const getMissingFileIds = (fileEntries, database) => {
       var needsPythonLoading = inputConfig[fileType].hasOwnProperty(
         "pythonLoader"
       );
-
       if (needsPythonLoading) {
-        return await pythonParseFileContents(filePathObj, saveFile);
+        var newFileObj = await pythonParseFileContents(filePathObj, event);
       } else {
-        return await parseFileContents(filePathObj, saveFile);
+        var newFileObj = await parseFileContents(filePathObj, saveFile);
       }
+      return newFileObj;
     })
   ).then(newJsonPathList => {
     //Return a list of new file IDs
-    return createFileDBEntry(database, newJsonPathList);
+    var missingDbEntries = fileEntries["missingDbEntries"];
+    return createFileDBEntry(database, missingDbEntries);
   });
 };
 
@@ -98,7 +217,7 @@ const saveFile = async (results, param) => {
   var json = JSON.stringify(results);
   var fileName = getRandomJsonFileName();
 
-  var filePath = rootPath + fileName;
+  var filePath = tempPath + fileName;
 
   return new Promise((resolve, reject) => {
     fs.writeFile(filePath, json, function(err) {
@@ -137,13 +256,15 @@ function sortFileEntries(db, filePathObject) {
 }
 
 //Returns a parsed object with all pathNames
-function getParsedFilePathObj(filePathObj) {
+function getParsedFilePathObj(filePathObj, event) {
   var parsedFilePathObjList = [];
+
   Object.keys(filePathObj).map(input => {
     filePathObj[input].map(filePath => {
       parsedFilePathObjList = [...parsedFilePathObjList, {pathName: filePath}];
     });
   });
+
   return parsedFilePathObjList;
 }
 //Create a new database analysis entry
@@ -169,6 +290,12 @@ export function getAllAnalysis(db) {
     allFiles: allFiles
   };
 }
+
+//Create a new db entry for a new yaml version
+function createDbYamlVersion(db, results) {
+  db.versions.insert(results);
+}
+
 //Create a relation between a file and an analysis
 function createDbRelations(db, results) {
   //For each file, relate it back to 1 analysisID
@@ -186,11 +313,12 @@ function createDbRelations(db, results) {
 }
 
 //Create a new database analysis entry
-async function getAnalysisID(db, name, description) {
+async function getAnalysisID(db, name, description, jiraId) {
   return new Promise(resolve => {
     var analysisObj = {
       name: name,
-      description: description
+      description: description,
+      jiraId: jiraId
     };
     var analysisId = db.analysis.insert(analysisObj);
     resolve(analysisId.$loki);
@@ -201,8 +329,7 @@ async function getAnalysisID(db, name, description) {
 function createFileDBEntry(db, newEntries) {
   var formattedFileEntries = newEntries.map(entry => {
     return {
-      pathName: entry.pathName,
-      jsonPath: entry.jsonPath
+      pathName: entry.pathName
     };
   });
 
